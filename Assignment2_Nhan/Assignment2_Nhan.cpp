0065fe403663120ee39c6b941858301ee349f2d5
btran <nguyenbao1003@outlook.com>
2022-02-21 19:05:52 -0500

    question6 enhance sin with tolerance

--- Assignment2_Nhan/Assignment2_Nhan.cpp
+++ Assignment2_Nhan/Assignment2_Nhan.cpp
@@ -3,10 +3,10 @@
 #include <cmath>
 using namespace std;
 
-unsigned long long int factorial(int num);
-long double maclaurinSin(double x, int N); // x is in radian, N is number of iteration
-void test(double data[], int dataSize, int terms[], int termsSize);
-
+unsigned long long int factorial(int num); // question 1
+long double maclaurinSin(double x, int N); // question 2 x is in radian, N is number of iteration
+void test(double data[], int dataSize, int terms[], int termsSize); // question 3,4
+long double enhancedMaclaurinSin(double x, int N); // question 6
 int main()
 {
   double testData[3] = {M_PI / 3, M_PI_4, M_PI / 6};
@@ -37,6 +37,19 @@ long double maclaurinSin(double x, int N)
   for (int i = 0; i < N; i++)
   {
     res += pow(-1, i) * pow(x, 2 * i + 1) / factorial(2 * i + 1);
+  }
+  return res;
+}
+long double enhancedMaclaurinSin(double x, int N)
+{
+  long double res = 0;
+  for (int i = 0; i < N; i++)
+  {
+    res += pow(-1, i) * pow(x, 2 * i + 1) / factorial(2 * i + 1);
+    if (i > 8 && abs(sin(x) - res) <= 0.00000000000000001)
+    {
+      break;
+    }
   }
   return res;
 }
